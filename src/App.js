8e63824e6b80315b5e091bc19d9337ec5783969f
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2024-08-23 20:05:15 -0300

    AULA 06-07

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 
 import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Category, {categories, filterCategory} from "./components/Category";
+import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Container from "./components/Container";
-import Card from "./components/Card"
 
 function App() {
   return (
@@ -11,14 +12,34 @@ function App() {
       <Header />
       <Banner image="home" />
       <Container>
-        <h2>Séries</h2>
+   
+      {categories.map((category, index) =>
+        <Category category={category}>
+          {filterCategory(index).map((video) => <Card id={video.id} key={video.id}/>)}
+        </Category>
+        )}
+
+        {/* <h2>Séries</h2>
         <section className="cards">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-        </section>
+          {videos.map((video) => <Card id={video.id} key={video.id} />)}
+        </section> */}
+      
+          {/* <Category category={categories[0]}>
+            {filterCategory(0).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[1]}>
+            {filterCategory(1).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[2]}>
+            {filterCategory(2).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[3]}>
+            {filterCategory(3).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category>
+          <Category category={categories[4]}>
+            {filterCategory(4).map((video) => <Card id={video.id} key={video.id} />)}
+          </Category> */}
+          
       </Container>
       <Footer />
     </>
