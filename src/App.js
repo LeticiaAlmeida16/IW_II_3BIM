6aef2c0a8308fc4c1bfe1df54b95345413f578aa
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2024-09-05 09:12:31 -0300

    aula08

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-
-import Banner from "./components/Banner";
-import Card from "./components/Card";
-import Category, {categories, filterCategory} from "./components/Category";
-import Container from "./components/Container";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from './pages/Home'
+import AppRoutes from "./routes";
 
 function App() {
   return (
-    <Home />
+    <AppRoutes  />
   );
 }
 
--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,8 +1,21 @@
+import Container from "../../components/Container";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import styles from "./Watch.module.css";
+
 function Watch(){
     return (
-        <section> 
-            
-        </section>
+        <>
+            <Header />
+            <Container> 
+                <section className={styles.watch}> 
+                    <h1>Assistir</h1>
+                    <iframe width="854" height="480" src="https://www.youtube.com/embed/DK3d0Pcc5MI?si=r_r3OUugqjgYogtY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </section>
+            </Container>
+            <Footer/>
+
+        </>
     );
 }
 
