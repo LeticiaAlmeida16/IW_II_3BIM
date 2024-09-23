0e9edfda1302c2b897c009a4224f26ab6adc448e
LeticiaAlmeida16 <leticia.almeidaaa2008@gmail.com>
2024-09-23 17:05:49 -0300

    aula14

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,13 @@
-import styles from "./Card.module.css"
+import { Link } from "react-router-dom";
+import styles from "./Card.module.css";
+
 function Card({id}){
     return(
         <section className={styles.card}> 
-            <a
-                href={`https://www.youtube.com/watch?v=${id}`}
-                rel="noreferrer noopener" 
-                target='_blank'>
+            <Link to={`/watch/${id}`}>
                 <img src={`https://i.ytimg.com/vi/${id}/mqdefault.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBG3kar6sgX0id-vAe-lNe1SwfWfA`} 
                 alt="Capa"/>
-            </a>
+            </Link>
         </section>
     );
 }
